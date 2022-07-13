8031cd12b04f46eb8fcd08cb196e887b7cff623c
Yiming Lin <yiminglin@fb.com>
2022-07-13 16:46:02 +0000

    change default configs

--- scripts/schedule.sh
+++ scripts/schedule.sh
@@ -2,6 +2,5 @@
 # Shedule execution of many runs
 # Run from root folder with: bash scripts/schedule.sh
 
-python train.py trainer.max_epochs=5
-
-python train.py trainer.max_epochs=10 logger=csv
+python train.py experiment=fairface-dex model.backbone.model_name=resnet18,resnet34 -m
+python train.py experiment=fairface-coral model.backbone.model_name=resnet18,resnet34 -m
